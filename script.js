0bcbc628d5b2e3494f8794cd4b2de5b47df8258d
Alizey <alizeycheema@gmail.com>
2021-07-17 12:55:50 +0400

    Pushing the calculator

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    amount= document.getElementById("principal").value;
+	interest= document.getElementById("rate").value;
+	noofyrs=document.getElementById("years").value;
+	total=((interest * amount)/100)* noofyrs;
+	
+	year=2020 + parseInt(noofyrs);
+	document.getElementById("demo").innerHTML = "If you deposit "  +  amount+ "<br>" +
+	"at an interest rate of " + interest + "<br>" + "You will receive an amount of " + total +"<br>"+"in the year " + year;
     
 }
-        
+      
